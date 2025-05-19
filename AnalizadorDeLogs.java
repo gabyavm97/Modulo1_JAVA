b4ac624006bbea226561dde2482cd5df9ea5fbf6
gabyavm97 <103579787+gabyavm97@users.noreply.github.com>
2025-05-19 09:16:01 -0600

    Reto14

---
+++ AnalizadorDeLogs.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class AnalizadorDeLogs {
+    public static void main (String [] args){
+        Path ruta = Paths.get ("errores.log");
+        try(BufferedReader lector = Files.newBufferedReader(ruta)){
+            String linea;
+            while ((linea = lector.readLine()) != null){
+                System.out.println("->"+linea);
+            }
+        } catch (IOException e){
+            System.out.println("Error al procesar el archivo"+e.getMessage());
+        }
+    }
+}
